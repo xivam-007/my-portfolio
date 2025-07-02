645b9846015110c04db959dc5607e98b5f58771d
xivam-007 <xivam007@gmail.com>
2025-07-02 22:56:42 +0530

    Fix: set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [
     react(),
     tailwindcss(),
